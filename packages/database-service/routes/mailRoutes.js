d513f1b509fc65045acaa75061a46ab39f5744a8
anotherFeng <regnagleppod@gmail.com>
2018-08-17 00:56:37 -0700

    add get mails request handlers

--- packages/database-service/routes/mailRoutes.js
+++ packages/database-service/routes/mailRoutes.js
@@ -1,19 +1,51 @@
 const router = require("express").Router();
 const { Mail } = require("../models/Mail");
 
-router.get('/', async (_, res) => {
-  const newMail = new Mail({
-    subject: "Hello",
-    receiver: "test@test.com",
-    content: "hello content"
-  });
-  await newMail.save();
-  res.send("new mail saved!");
-})
+const pingHandler = (req, res) => {
+  res.send("healthy");
+};
 
-router.get('/test', async (req, res) => {
-  const mails = await Mail.find();
-  res.send(mails);
-})
+const mailHandler = async (req, res) => {
+  let mails;
+  let error;
+  try{
+    mails = await Mail.find();
+  } catch (err) {
+    error = err;
+  };
+
+  res.send({
+    message: "Got response from db",
+    service: "Database Service",
+    status: 200,
+    payload: mails || error
+  });
+};
+
+const singleMailHandler = async (req, res) => {
+  const {id} = req.params;
+  console.log(id)
+  let mail;
+  let error;
+  try {
+    mail = await Mail.findById(id)
+  } catch (err) {
+    error = err;
+  };
+
+  res.send({
+    message: "Got response from db",
+    service: "Database Service",
+    status: 200,
+    payload: mail || error
+  });
+}
+
+router.route('/')
+  .get(pingHandler)
+router.route('/mails')
+  .get(mailHandler)
+router.route('/mails/:id')
+  .get(singleMailHandler)
 
 module.exports = router;
