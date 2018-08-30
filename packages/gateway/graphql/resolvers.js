4c486cd261009b897f8e62bea7d1f8457fb6b2db
Feng Desktop <regnagleppod@gmail.com>
2018-08-30 01:21:12 -0400

    add resolver to fetch specific mail by id

--- packages/gateway/graphql/resolvers.js
+++ packages/gateway/graphql/resolvers.js
@@ -3,12 +3,17 @@ const axios = require('axios');
 const getMails = async () => {
   const mails = (await axios.get('http://localhost:4001/api/v1/database/mails')).data.payload;
   return mails;
-}
+};
+
+const getMail = async (id) => {
+  const mail = (await axios.get(`http://localhost:4001/api/v1/database/mails/${id}`)).data.payload;
+  return mail;
+};
 
 module.exports = {
   Query: {
     mails: () => getMails(),
-    mail: (root, args, context) => console.log(args, context)
+    mail: (root, args, context) => getMail(args.id)
   },
   Mutation: {
     mail: (_, args) => {
