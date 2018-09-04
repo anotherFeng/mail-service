ee3a5abf4af41db2890fa028a84cb5330f31b20a
anotherFeng <regnagleppod@gmail.com>
2018-09-04 03:32:44 -0400

    setup mailjet request handler

---
+++ packages/mailing-service/handler/sendMail.js
@@ -0,0 +1,20 @@
+const { mail: { apiPublic, apiSecret }} = require('../config');
+const Mailjet = require('node-mailjet').connect(apiPublic, apiSecret);
+
+console.log(Mailjet);
+
+module.exports = async (mail) => {
+  const request = await Mailjet.post('send').request({
+    FromEmail: 'regnagleppod@gmail.com',
+    FromName: 'Feng',
+    Subject: mail.subject,
+    'Text-part': mail.content,
+    Recipients: [
+      {
+        Email: mail.receiver
+      }
+    ]
+  });
+
+  console.log(request.body)
+}
